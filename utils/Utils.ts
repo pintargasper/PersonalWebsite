01634d07557ca4f1e4e1cbb6c22399ac20e2b96e
Gašper Pintar <pintargasper1@gmail.com>
2025-10-28 12:14:48 +0100

    Update Config

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -1,12 +1,15 @@
+import * as fs from "node:fs";
+import path from "node:path";
+
 interface Version {
     versions: Record<string, string>;
 }
 
 const getLatestVersion: (appKey: string) => Promise<string> = async (appKey: string): Promise<string> => {
     try {
-        const baseUrl: string | undefined = process.env.NEXT_PUBLIC_BASE_URL;
-        const response: Response = await fetch(`${baseUrl}/projects/versions.json`);
-        const data: Version = await response.json();
+        const filePath: string = path.join(process.cwd(), "public/projects/versions.json");
+        const fileData: string = fs.readFileSync(filePath, "utf-8");
+        const data: Version = JSON.parse(fileData);
         return data.versions[appKey] ?? "N/A";
     } catch {
         return "N/A";
