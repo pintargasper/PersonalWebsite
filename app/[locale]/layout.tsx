a99bd619cde77789a447e1b5da50a5dd9ea87e86
Gašper Pintar <pintargasper1@gmail.com>
2025-11-20 14:45:04 +0100

    Update Google Analytics script

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,12 +4,10 @@ import Navigation from "@/app/[locale]/components/Navigation";
 import CookieNotice from "@/app/[locale]/components/CookieNotice";
 import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
 import {NextIntlClientProvider, hasLocale} from "next-intl";
-
+import { GoogleAnalytics } from "@next/third-parties/google"
 import { config } from "@fortawesome/fontawesome-svg-core";
 import {Viewport} from "next";
-import Script from "next/script";
 import {routing} from "@/i18n/routing";
 import {notFound} from "next/navigation";
 config.autoAddCss = false;
@@ -41,19 +39,7 @@ async function RootLayout({children, params}: {
               data-scroll-behavior={"smooth"}
         >
         <head>
-            {/* Google Analytics */}
             <meta name="google-site-verification" content="ful3dtJx-2GOrhDrAbYsMI2oXsYASfAWbXLXZSj6Gf0"/>
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-06PV0WY17C"
-            />
-            <Script id="google-analytics">
-                {`
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', 'G-06PV0WY17C');
-                        `}
-            </Script>
         </head>
         <body>
         <NextIntlClientProvider>
@@ -66,6 +52,7 @@ async function RootLayout({children, params}: {
                 <Footer />
             </div>
         </NextIntlClientProvider>
+        <GoogleAnalytics gaId={"G-06PV0WY17C"} />
         </body>
         </html>
     );
